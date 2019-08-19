721518828330edd012b5fb154f969cdabf70c155
Leandro Ribeiro <lribeir1@binghamton.edu>
2019-08-18 22:15:06 -0400

    Pings server directly instead of using outside API

--- mcbot.js
+++ mcbot.js
@@ -1,70 +1,48 @@
-//credit to vegeta897 for the request URL part from his 'Simple Minecraft server status bot'
 const Discord = require("discord.js");
+const mcping = require('mc-ping-updated');
 const client = new Discord.Client();
 const settings = require('./config.json');
-var statustring = "No signal";
 
-var request = require('request');
-var mcCommand = '/minecraft'; // Command for triggering
 var mcIP = settings.ip; // Your MC server IP
 var mcPort = settings.port; // Your MC server port
 
-var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
-
-
 function update() {
-  /*seconds = seconds + 1;
-  secondsString = seconds.toString();
-  client.user.setActivity(secondsString, { type: 'Playing' })
-  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch(console.error);*/
-  request(url, function(err, response, body) {
-      if(err) {
-          console.log(err);
-          //return message.reply('Error getting Minecraft server status...');
-      }
-      body = JSON.parse(body);
-      var status = 'Server offline';
-      console.log(body.motd);
-      if(body.online) {
-          if((body.motd=="&cWe are under maintenance.")||(body.players.now>=body.players.max)){
-            client.user.setStatus('idle')
-            //.then(console.log)
-            .catch(console.error);
-          }else{
-            client.user.setStatus('online')
-            //.then(console.log)
-            .catch(console.error);
-          }
-            if(body.players.now) {
-                status = ' ' + body.players.now + '  of  ' + body.players.max;
-              } else {
-                status = ' 0  of  ' + body.players.max;
-        }
-      } else {
-        client.user.setStatus('dnd')
-        //.then(console.log)
-        .catch(console.error);
 
-      }
-      client.user.setActivity(status, { type: 'PLAYING' })
-      .then(presence => console.log(status))
-      .catch(console.error);
-  });
+	mcping(mcIP, function(err, res) {
+		var status = 'Server offline';
+		if (err) {
+			console.error(err);
 
+			client.user.setStatus('dnd')
+			//.then(console.log)
+				.catch(console.error);
+
+		} else {
+			// Success!
+			console.log(res.description.text);
+			if((res.description.text =="&cWe are under maintenance.")||(res.players.online>=res.players.max)){
+				client.user.setStatus('idle')
+				//.then(console.log)
+					.catch(console.error);
+			}else{
+				client.user.setStatus('online')
+				//.then(console.log)
+					.catch(console.error);
+			}
+
+			status = ' ' + res.players.online + '  of  ' + res.players.max;
+		}
+
+		client.user.setActivity(status, { type: 'PLAYING' })
+			.then(presence => console.log(status))
+			.catch(console.error);
+
+	});
 }
+
 client.on("ready", () => {
-  console.log("I am ready!");
-  client.setInterval(update,30000);
+	console.log("I am ready!");
+	client.setInterval(update,5000);
 });
-
-/*client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-    update();
-
-  }
-}
-);*/
 
 client.login(settings.token);
